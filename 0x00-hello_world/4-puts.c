25562fcc21f864919bb3d1aab06b9eb7ed1a0363
laamari71 <elaamarisoulaiman@gmail.com>
2023-03-16 20:03:11 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main-Entry point
- *
- *
- * Return: Always 0 (Success)
+ * Desription: main - Prints out a string to stdout.
+ * return: 0 if success.
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+int main(avoid)
+(
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
