9dc656a78623f6f4586f3ce834b2230ff245d633
laamari71 <elaamarisoulaiman@gmail.com>
2023-03-23 11:14:18 -0700

    0-main.c

--- 0x04-more_functions_nested_loops/0x03-debugging/0-main.c
+++ 0x04-more_functions_nested_loops/0x03-debugging/0-main.c
@@ -3,8 +3,6 @@
 *main - tests function that prints if integer is positive or negative
 *Return: 0
 */
-
-
 int main(void)
 
 {
@@ -12,7 +10,5 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
-
