22e48fcbcad88037ff1cfb4fd50ce05fb35ddfcb
laamari71 <elaamarisoulaiman@gmail.com>
2023-04-09 10:10:44 -0700

    create_static_lib.sh

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _isdigit(int c)
+{
+    return isdigit(c);
+}
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _strlen(char *s)
+{
+    return strlen(s);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+char *_strcpy(char *dest, char *src)
+{
+    return strcpy(dest, src);
+}
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int _atoi(char *s)
+
+{
+    return atoi(s);
+}
+
+
